2794bd3cdbce59c450752828f0fc98ac53f6df9b
ranapk89 <ranapk89@gmail.com>
2020-07-29 12:14:05 +0530

    images in grid

--- App.js
+++ App.js
@@ -1,58 +1,73 @@
-import React, { Component } from 'react';
-import { View, Image, FlatList, PermissionsAndroid, Platform } from 'react-native';
-import CameraRoll from "@react-native-community/cameraroll";
+import React, {Component} from 'react';
+import {
+  View,
+  Image,
+  FlatList,
+  PermissionsAndroid,
+  Platform,
+  TouchableHighlight,
+} from 'react-native';
+import CameraRoll from '@react-native-community/cameraroll';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        data:''
+      data: '',
+      selectedPhotos: '',
     };
   }
 
-  async componentDidMount(){
+  async componentDidMount() {
     if (Platform.OS === 'android') {
-        const result = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-          {
-            title: 'Permission Explanation',
-            message: 'ReactNativeForYou would like to access your photos!',
-          },
-        );
-        if (result !== 'granted') {
-          console.log('Access to pictures was denied');
-          return;
-        }
+      const result = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        {
+          title: 'Permission Explanation',
+          message: 'ReactNativeForYou would like to access your photos!',
+        },
+      );
+      if (result !== 'granted') {
+        console.log('Access to pictures was denied');
+        return;
       }
+    }
 
-      CameraRoll.getPhotos({
-        first: 50,
-        assetType: 'Photos',
-      })
-      .then(res => {
-        this.setState({ data: res.edges });
+    CameraRoll.getPhotos({
+      first: 1000,
+      assetType: 'Photos',
+    })
+      .then((res) => {
+        this.setState({data: res.edges});
       })
       .catch((error) => {
-         console.log(error);
+        console.log(error);
       });
-    
   }
-
+  add = (item) => {
+    console.log(item);
+  };
   render() {
     return (
-      <View>
-        <FlatList
-        data={this.state.data}
-        numColumns={3}
-        renderItem={({ item }) => <Image
-           style={{
-             width: '33%',
-             height: 150,
-           }}
-           source={{ uri: item.node.image.uri }}
-         />}
-      />
-      </View>
+      <>
+        <View style={{flex: 1}}></View>
+        <View style={{flex: 1}}>
+          <FlatList
+            data={this.state.data}
+            numColumns={3}
+            renderItem={({item}) => (
+              <Image
+                resizeMode="contain"
+                style={{
+                  width: '33%',
+                  aspectRatio: 1 / 1,
+                }}
+                source={{uri: item.node.image.uri}}
+              />
+            )}
+          />
+        </View>
+      </>
     );
   }
 }
